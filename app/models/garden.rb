fdaca77984ef069d0e972ffba4e58634911df9a8
Denis Fortict <d.fortict@vivit.fr>
2019-02-18 19:12:54 +0100

    add create and new method in garden controller _ modification validates in garden model _ simple_form_for in new_html.erb

--- app/models/garden.rb
+++ app/models/garden.rb
@@ -1,10 +1,10 @@
 class Garden < ApplicationRecord
   has_many :bookings
   belongs_to :user
-  validates :title
-  validates :address
-  validates :description
-  validates :surface
-  validates :price
-  validates :product, inclusion: {in:["tomates", "salades", "haricots", "choux", "radis", "courgettes"]}
+  validates :title, presence: true
+  # validates :address
+  # validates :description
+  # validates :surface
+  # validates :price
+  # validates :product, inclusion: {in:["tomates", "salades", "haricots", "choux", "radis", "courgettes"]}
 end
