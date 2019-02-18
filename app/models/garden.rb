61048b478eafdd0ef4ffad14b31c8e232f2611a7
Denis Fortict <d.fortict@vivit.fr>
2019-02-18 18:18:27 +0100

    add garden validates in garden model

--- app/models/garden.rb
+++ app/models/garden.rb
@@ -1,4 +1,10 @@
 class Garden < ApplicationRecord
   has_many :bookings
   belongs_to :user
+  validates :title
+  validates :address
+  validates :description
+  validates :surface
+  validates :price
+  validates :product, inclusion: {in:["tomates", "salades", "haricots", "choux", "radis", "courgettes"]}
 end
