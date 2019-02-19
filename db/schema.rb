fcf6b1925ace9fdfa5ca18807f5028a96718b981
Jeanne Bouaziz <jeannebouaziz@hotmail.fr>
2019-02-19 18:45:32 +0100

    Some front added for gardens#show

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_19_135653) do
+ActiveRecord::Schema.define(version: 2019_02_19_170209) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 2019_02_19_135653) do
     t.string "first_name"
     t.string "last_name"
     t.string "city"
+    t.string "photo"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
