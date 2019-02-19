f8b1126d08f86f2c2299d6f8b5a9e3844cd896f0
Astrid Delcros <astriddelcros@gmail.com>
2019-02-19 15:46:35 +0100

    dashboard

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -5,6 +5,10 @@ class BookingsController < ApplicationController
     authorize @booking
   end
 
+  def my_bookings
+    @my_bookings = Booking.where(user_id: current_user)
+  end
+
   def create
     @booking = Booking.new(booking_params)
     authorize @booking
