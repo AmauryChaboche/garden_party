e42e7470040076b261bc215837a2cfadc9a25482
Jeanne Bouaziz <jeannebouaziz@hotmail.fr>
2019-02-21 18:39:26 +0100

    flatpickr and JS added

--- app/javascript/components/countDays.js
+++ app/javascript/components/countDays.js
@@ -1,15 +1,19 @@
 const countDays = () => {
-  const start_date = document.getElementById('booking_start_date_3i')
-  const end_date = document.getElementById('booking_end_date_3i')
+  const start_date = document.getElementById('booking_start_date')
+  const end_date = document.getElementById('booking_end_date')
+
   const field = document.getElementById('number-days')
   const garden_price = document.getElementById('garden-price')
 
+console.log(start_date.value)
+console.log(end_date.value)
+
   if (start_date) {
-    start_date.addEventListener('change', (event) => {
-      field.innerText = (end_date.value - start_date.value) * parseInt(garden_price.innerText, 10)
-    })
     end_date.addEventListener('change', (event) => {
-      field.innerText = (end_date.value - start_date.value) * parseInt(garden_price.innerText, 10)
+      const start = new Date(start_date.value);
+      const end = new Date(end_date.value);
+      const count = (end - start) / 60000 / 1440;
+      field.innerText = count * parseInt(garden_price.innerText, 10)
     })
   }
 }
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,6 +4,7 @@ import Swal from 'sweetalert2'
 
 import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
 import { initMapbox } from '../plugins/init_mapbox';
+import "../plugins/flatpickr"
 
 initMapbox();
 
